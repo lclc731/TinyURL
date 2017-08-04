bb6c8ad0fd2a5f2d275c5eba650492bcbf8a2acd
ChangLiu <lclc731@gmail.com>
2017-08-04 13:22:41 -0700

    Add bootstrap and angular to the frontend

---
+++ public/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ChangLiu on 8/4/17.
+ */
+var app = angular.module('tinyurlApp', []);
--- server.js
+++ server.js
@@ -8,10 +8,16 @@ var app = express();
 
 var restRouter = require('./routes/rest');
 var redirectRouter = require('./routes/redirect');
-// var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
+
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.use("/api/v1", restRouter);
+
+app.use("/", indexRouter);
+
 app.use("/:shortUrl", redirectRouter);
-// app.use("/", indexRouter);
+
 
 app.listen(3000);
